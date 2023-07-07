b43d460f102c193cd08f5587dd31949279cfe2dc
sarobidy13 <sarobidyfitahiana01@gmail.com>
2023-07-07 19:57:20 +0200

    sprint8

--- TestFramework/Test.java
+++ TestFramework/Test.java
@@ -1,19 +1,41 @@
-import etu1816.framework.ModelView;
 
+import etu1816.framework.ModelView;
 import etu1816.framework.MethodAnnotation;
 
 public class Test {
 
-    String nom;
+    private int id;
+    private String nom;
 
     @MethodAnnotation(url = "save.do")
-    public ModelView test() {
+    public ModelView save() {
         ModelView mv = new ModelView();
 
         mv.setView("/test.jsp");
-        mv.addItem("test", this.getNom());
+        mv.addItem("obj", this);
 
         return mv;
+    }
+
+    @MethodAnnotation(url = "setNewTest.do", paramName = "identifiant,name")
+    public ModelView newTest(int identifiant, String name) {
+        ModelView mv = new ModelView();
+
+        this.setId(identifiant);
+        this.setNom(name);
+
+        mv.setView("/test.jsp");
+        mv.addItem("obj", this);
+
+        return mv;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNom() {
